0a365a4ad808d3ba1a0ed228a533e3c22ea09ef8
Feng Sao <wildsaofeng@gmail.com>
2019-06-06 00:28:50 +0800

    test

--- indexByWeb3.js
+++ indexByWeb3.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var moment = require('moment');
 
 const startBlock = 1900000;
-const endBlock = 2000000;
+const endBlock = 1900010;
 
 // Part  Setting Up DB Model
 const ethContractSchema = mongoose.Schema({
