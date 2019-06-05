b28f1bd39f4e47d378a89213a48481185d82c4d1
Feng Sao <wildsaofeng@gmail.com>
2019-06-05 23:04:04 +0800

    add basic coding file

---
+++ indexByWeb3.js
@@ -0,0 +1,97 @@
+// Part 1 Import Related Package
+var Web3 = require('web3');
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+const maxBlock = 100000;
+
+// Part  Setting Up DB Model
+const ethContractSchema = mongoose.Schema({
+  address: String,
+  code: String,
+  storage: String,
+});
+
+const EthContract = mongoose.model('eth_contract', ethContractSchema);
+
+EthContract.addContract = function(newContract, callback) {
+  newContract.save(callback);
+};
+
+EthContract.updateContract = function(contractAddress, newCode, newStorage) {
+  EthContract.update({address: contractAddress}, { $set: {
+    "code": newCode,
+    "storage": newStorage,
+    }}, (err, suc) => {});
+}
+
+EthContract.checkUnique = function(contractAddress) {
+  EthContract.find({address: contractAddress}, (err, res) => {
+    if(res) return false;
+    return true;
+  });
+};
+
+// Part  Connection to Local RPC
+
+async function connectWeb3() {
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  global.web3 = web3;
+}
+
+async function connectDB() {
+  mongoose.connect('mongoose://localhost:27017/ethscanner');
+  mongoose.connection.on('connected', () => {
+   console.log('MongoDB has started successfully.');
+  });
+  mongoose.connection.on('error', (err) => {
+   console.log('Database error' + err);
+  });
+}
+
+// 
+function checkIfIsContract(newAddress) {
+  var code = web3.eth.getCode(newAddress);
+  if(code === '0x')
+    return false;
+  else return true;
+}
+
+async function uniqueAddAddress(newAddress) {
+  if(!checkIfIsContract(newAddress))
+    return;
+  if(EthContract.checkUnique(newAddress)){
+    EthContract.addContract(new ethContractSchema({
+      address: newAddress,
+    }));
+  }
+}
+
+// Part  Scan the Whole Chain
+
+async function scanTheChain() {
+  for(let i = 1; i <= maxBlock; i++) {
+    var blockTxes = web3.eth.getBlock(i).transactions;
+    var blockTxCnt = blockTxes.length;
+    console.log("[ " + moment() +" ] " + "Scanning Block " + i);
+    for(let j = 1; j <= blockTxCnt; j++) {
+      var thisTx = web3.eth.getTransaction(blockTxes[j]);
+      const txFrom = thisTx.from;
+      uniqueAddAddress(txFrom);
+      const txTo = thisTx.to;
+      uniqueAddAddress(txTo);
+
+    }
+  }
+}
+
+
+
+(function main(){
+  moment.locale('zh-cn');
+  moment().format('MMMM Do YYYY, h:mm:ss a');
+  connectDB();
+  connectWeb3();
+  scanTheChain();
+})();
+
---
+++ init.sh
@@ -0,0 +1,4 @@
+npm install levelup leveldown rlp merkle-patricia-tree --save
+npm install ethereumjs-util ethereumjs-account --save
+npm install web3 --save
+npm install mongoose --save
